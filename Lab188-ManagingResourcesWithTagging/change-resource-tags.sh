3dc6405c06bb0f9a2a226d2a6ed18783bfa5a163
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2022-09-06 15:56:01 +0700

    Update change-resource-tags.sh

--- Lab188-ManagingResourcesWithTagging/change-resource-tags.sh
+++ Lab188-ManagingResourcesWithTagging/change-resource-tags.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-ids=$(aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem" "Name=tag:Environment,Values=development" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+ids=$(aws ec2 describe-instances \
+       --filter "Name=tag:Project,Values=ERPSystem" "Name=tag:Environment,Values=development" \
+       --query 'Reservations[*].Instances[*].InstanceId' \
+       --output text)
 
-aws ec2 create-tags --resources $ids --tags 'Key=Version,Value=1.1'
+aws ec2 create-tags \
+   --resources $ids \
+   --tags 'Key=Version,Value=1.1'
